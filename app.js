819780fbeb88d427fc51c98f2ae0dbb46ae70570
Theaceupthesleeve <gamebreaker142007@gmail.com>
2024-08-16 05:49:19 -0700

    chapter 4 complete

---
+++ app.js
@@ -0,0 +1,618 @@
+
+
+
+
+// console.log("Hello world");
+
+
+
+// function Hello(){
+//     console.log("helloww world");
+    
+// }
+
+
+/////////////////////////////// CHAPTER ONE //////////////////////////////
+
+// alert("Hello user")
+
+
+// alert("Error! Please enter valid password")
+
+// alert("Welcome to JS Land.. \n Happy coding!")
+
+
+// alert("Welcome to JS Land...") 
+
+// alert("Happy Coding! \n prevent this page from creating addtional dialogs.")
+
+
+
+
+
+/////////////////////// COMPLETE ////////////////////////////////// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////// CHAPTER TWO /////////////////////////////////
+
+
+
+// var username = "Jeremiah"
+
+
+
+// alert(username)
+
+
+
+
+// var myName = "Jeremiah John"
+
+
+
+
+
+// alert(myName)
+
+
+
+
+// var message = "Hello world"
+
+
+
+
+// alert(message)
+
+
+
+
+
+
+
+
+
+
+
+
+// var username = "Mr. chai wala"
+// var age = "old enough"
+// var qulification = "CHAIII!!!"
+
+
+
+
+
+
+
+
+
+
+
+
+// alert(username);
+// alert(age);
+// alert(qulification);
+
+
+
+
+
+
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+
+
+
+
+
+
+
+
+
+
+// var email = " gamebreaker142007@gmail.com"
+
+
+
+
+
+
+// alert("my email is" + email)
+
+
+
+
+
+// var theBook = "A smarter way to learn JavaScript"
+
+
+
+
+
+
+
+// alert("I am trying to learn JavaScript from the Book " + theBook)
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector("#head");
+
+// head.innerHTML = "Yes! i can change HTML through JavaScript"
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+//------------------------------  COMPLELTE -------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////// CHAPTER THREE ////////////////////////////////////////
+
+
+// var age = 15
+
+// alert("I am " + age + " years old")
+
+
+// var visitors = 18
+
+// alert("you have visisted this website " + visitors + " times")
+
+
+// var birthYear = 2007
+// var head = document.querySelector("#head");
+
+// head.innerHTML = "My birth year is " + birthYear + "<br /> Data type of my declared variable is number"
+
+
+
+
+// var userName = "Freddy"
+// var orders = 5 + "T-shirt(s)"
+
+// var head = document.querySelector('#head');
+
+// head.innerHTML = userName + " ordered " + 5 + " T-shirt(s) on XYZ Clothing store "
+
+
+//------------------------------  COMPLELTE -------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////// CHAPTER FOUR ////////////////////////////////////////
+
+
+// var head = document.querySelector('#head'); 
+
+// head.innerHTML = "Rules for naming JS Variables"
+
+// var text = document.querySelector('h2')
+
+// text.innerHTML = "Variable names can only contain, $ and _ . For example  1$my_Variable <br /> Variables must begin with a letter, $ or _. For example: $name, _name or name <br /> Variable names are case Sensitive <br / > Variable names should not be JS Keywords"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 3 
+// var num2 = 5
+
+// var total = num1 + num2
+
+// var head = document.querySelector('#head'); 
+
+// head.innerHTML = "the sum of " + num1 + " plus " + num2 + ' is ' + total 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 3 
+// var num2 = 5
+
+// var total = num1 - num2
+
+// var head = document.querySelector('#head'); 
+
+// head.innerHTML = "the sum of " + num1 + " subtract " + num2 + ' is ' + total 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 3 
+// var num2 = 5
+
+// var total = num1 * num2
+
+// var head = document.querySelector('#head'); 
+
+// head.innerHTML = "the sum of " + num1 + " times " + num2 + ' is ' + total 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num1 = 3 
+// var num2 = 5
+
+// var total = num1 / num2
+
+// var head = document.querySelector('#head'); 
+
+// head.innerHTML = "the sum of " + num1 + " divided " + num2 + ' is ' + total 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var intValue = 5
+// var increment = intValue + 1
+// var addtion = increment + 7 
+// var decrement = addtion - 1 
+// var reminder = decrement / 3
+
+
+// var head = document.querySelector('#head');
+// head.innerHTML = "Value  after Variable declaration is undefined"
+
+// var text = document.querySelector("h2");
+// text.innerHTML = "Intial Value: " + intValue + 
+// "<br /> Value after increment is: " + increment +
+// "<br /> Value after addtion is: " + addtion + 
+// "<br /> Value after decrement is: " + decrement +
+// "<br /> The reminder is: " + reminder
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var ticket = 600
+// var people = 5 
+
+// var total = 600 * 5 
+
+// var head = document.querySelector("#head");
+// head.innerHTML = "Total cost of 5 tickets to a movie is " + total + "PKR"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector("#head");
+
+// head.innerHTML = "Table of 2 " + 
+// "<br />"+ " 2 X 1 = " + 2 * 1 + 
+// "<br />"+ " 2 X 2 = " + 2 * 2 +
+// "<br />"+ " 2 X 3 = " + 2 * 3 +
+// "<br />"+ " 2 X 4 = " + 2 * 4 + 
+// "<br />"+ " 2 X 5 = " + 2 * 5 + 
+// "<br />"+ " 2 X 6 = " + 2 * 6 + 
+// "<br />"+ " 2 X 7 = " + 2 * 7 + 
+// "<br />"+ " 2 X 8 = " + 2 * 8 + 
+// "<br />"+ " 2 X 9 = " + 2 * 9  
+
+
+// var calcius = 25 + "°C" 
+// var _farenhite = 25 - 32 * 5 / 9 + "°F"
+
+// var head = document.querySelector("#head");
+// head.innerHTML = calcius + " is " + _farenhite +
+// "<br />" + _farenhite + " is " + calcius
+
+
+
+
+
+
+
+
+
+
+
+
+// var priceOf1 = 650 
+// var quantityOf1 = 3
+// var totalOf1 = priceOf1 * quantityOf1
+// var priceOf2 = 100
+// var quantityOf2 = 7
+// var totalOf2 = priceOf2 * quantityOf2
+// var shippingCharges = 100
+
+// var totalCost = totalOf1 + totalOf2 + shippingCharges
+
+// var head = document.querySelector("#head");
+// head.innerHTML = "Price of item 1 is " + priceOf1 +
+// "<br / > Quantity of item 1 is " + quantityOf1 +
+// "<br / > Price of item 2 is " + priceOf2 +
+// "<br / > Quantity of item 2 is " + quantityOf2 +
+// "<br / > Shipping charges are " + shippingCharges +
+// "<br />" +
+// "<br /> Total cost of your order is " + totalCost
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var totalMarks = 400
+// var maths = 97
+// var eng = 76
+// var chem = 77
+// var phys = 89
+// var obtainedMarks = eng + chem + maths + phys 
+// var total = obtainedMarks / totalMarks * 100
+
+// var head = document.querySelector("#head");
+// head.innerHTML = "MARKSHEET" +
+// "<br /> Total marks: " + totalMarks +
+// "<br /> Obtained marks: " + obtainedMarks +
+// "<br /> Percentaege: " + total 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var USD = 104.80
+// var riyal = 28 
+// var total = USD + riyal
+
+// var head = document.querySelector("#head");
+
+// head.innerHTML = "Currency in PKR" + 
+// "<br />" +
+// "<br />" +
+// "<br /> Total Currency in PKR is: " + total + "PKR"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var head = document.querySelector("#head");
+
+// head.innerHTML = (5 * 10) / 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var currentYear = 2024
+// var birthYear = 2007
+// var yourAge = birthYear - currentYear
+
+// var head = document.querySelector("#head");
+
+// head.innerHTML = "Current Year: " + currentYear +
+// "<br /> Birth Year: " + birthYear +
+// "<br /> Your age: " + yourAge
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
